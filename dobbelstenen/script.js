2d880b99c65756f8f4340e3059978febe8e278ee
Marcwaffle <veldejohanvander@gmail.com>
2026-08-06 14:28:06 +0200

    Did the Dice exercise

---
+++ dobbelstenen/script.js
@@ -0,0 +1,33 @@
+const button = document.getElementById("diceroll");
+
+
+function rollDice() {
+    let results = [];
+    let count = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
+    };
+    let elements = [...document.querySelectorAll(".countData")]
+    
+    for (let i = 0; i < 8; i++) {
+        let randomNumber = Math.floor(Math.random()* 6 + 1)
+        results.push(randomNumber);
+        count[randomNumber] ++
+    }
+    console.log(results, count);
+
+    elements.forEach((el, index) => {
+        el.textContent = count[index + 1];
+    });
+}
+
+
+if (button) {
+    button.addEventListener("click", rollDice);
+} else {
+    console.log("no button found");
+}
