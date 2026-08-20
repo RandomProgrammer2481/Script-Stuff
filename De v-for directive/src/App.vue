9be0dbe073ee59ee158ff40e3a526c1946063da1
RandomProgrammer2481 <veldejohanvander@gmail.com>
2026-08-20 15:56:23 +0200

    v-for directive klaar

---
+++ De v-for directive/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, reactive, computed } from "vue";
+
+const tasks = ref([
+  { name: "Boodschappen doen", completed: false },
+  { name: "Afwassen", completed: true },
+  { name: "Hond uitlaten", completed: false },
+]);
+</script>
+
+<template>
+  <h1>TakenLijst</h1>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="index">
+      {{ task.name }}
+      {{ task.completed ? "(voltooid)" : "(niet voltooid)" }}
+      <button @click="task.completed = !task.completed">Toggle</button>
+    </li>
+  </ul>
+</template>
