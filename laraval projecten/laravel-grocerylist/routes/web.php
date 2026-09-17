1578d50bf245f70ddce108798e238c2f7d9a3e74
RandomProgrammer2481 <veldejohanvander@gmail.com>
2026-09-17 15:29:36 +0200

    added controllers and some views, added necesary reroutring

--- laraval projecten/laravel-grocerylist/routes/web.php
+++ laraval projecten/laravel-grocerylist/routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/items', function () {})->name('items.index');
-Route::get('/items/create', function () {})->name('items.create');
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('/items', function () {})->name('items.store');
 Route::get('/items/{id}', function () {})->name('items.show');
 Route::get('/items/{id}/edit', function () {})->name('items.edit');
@@ -16,3 +13,7 @@ Route::delete('/items/{id}', function () {})->name('items.destroy');
 
 // We voegen ook een redirect toe aan de routes die de hoofdpagina doorverwijst naar de '/items' route
 Route::redirect('/', '/items');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
